6754404c44a0b263cca78b74628ec1b04959fd30
Christopher Blum <christopher.blum@profitbricks.com>
2018-12-17 17:43:14 +0100

    Even more optimisations to the "fill credentials" code
    Signed-off-by: Christopher Blum <christopher.blum@profitbricks.com>

--- content.js
+++ content.js
@@ -11,6 +11,7 @@ chrome.runtime.onMessage.addListener(
       for (let i = 0; i < inputs.length; i++) {
         if (inputs[i].type === 'password') {
           passwordNode = inputs[i]
+          break
         }
       }
       if (passwordNode === null) {
@@ -18,6 +19,7 @@ chrome.runtime.onMessage.addListener(
         return
       }
 
+      // Find the username field next to the password field
       for (let testNode = passwordNode.previousSibling; testNode !== null; testNode = testNode.previousSibling) {
         if (testNode && testNode.tagName && testNode.tagName.toUpperCase() === 'INPUT') {
           usernameNode = testNode
@@ -25,16 +27,29 @@ chrome.runtime.onMessage.addListener(
         }
       }
 
+      // Go upwards until we find the form node - then check all input nodes
       if (usernameNode !== null) {
         for (let form = passwordNode.parentElement; form !== null; form = form.parentElement) {
           if (form && form.tagName && form.tagName.toUpperCase() === 'FORM') {
             let inputElements = form.getElementsByTagName('input')
             for (let i = 0; i < inputElements.length; i++) {
-              if (inputElements[i].type === 'text') {
+              if (inputElements[i].type === 'text' || inputElements[i].type === 'email') {
                 usernameNode = inputElements[i]
                 break
               }
             }
+            break
+          }
+        }
+      }
+
+      // Go completely crazy and wild guess any visible input field for the username
+      // https://stackoverflow.com/a/21696585
+      if (usernameNode !== null) {
+        let inputElements = document.getElementsByTagName('input')
+        for (let i = 0; i < inputElements.length; i++) {
+          if (inputElements[i].offsetParent && (inputElements[i].type === 'text' || inputElements[i].type === 'email')) {
+            usernameNode = inputElements[i]
             break
           }
         }

a97256bac5aaeee71bda1464ac268236073921e7
Chris Blum <zeichenanonym@web.de>
2018-12-19 10:06:03 +0100

    Removed URL verification
    Signed-off-by: Chris Blum <zeichenanonym@web.de>

--- common.js
+++ common.js
@@ -29,15 +29,6 @@ function authToVault(vaultServer, username, password, authMount) {
   })
 }
 
-// verify URL function (copy pasted from google, standard one)
-function checkURL(value) {
-  var urlregex = new RegExp("^(http|https)://([a-zA-Z0-9.-]+(:[a-zA-Z0-9.&amp;%$-]+)*@)*((25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]).(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0).(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0).(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[0-9])|([a-zA-Z0-9-]+.)*[a-zA-Z0-9-]+.(com|edu|gov|int|mil|net|org|biz|arpa|info|name|pro|aero|coop|museum|[a-zA-Z]{2}))(:[0-9]+)*(/($|[a-zA-Z0-9.,?'\\+&amp;%$#=~_-]+))*$")
-  if (urlregex.test(value)) {
-    return (true)
-  }
-  return (false)
-}
-
 function authButtonClick() {
   var notify = document.getElementById('notify')
   console.debug('AUTH initiated!')
@@ -48,15 +39,10 @@ function authButtonClick() {
   var pass = document.getElementById('passBox')
   // verify input not empty. TODO: verify correct URL format.
   if ((vaultServer.value.length > 0) && (login.value.length > 0) && (pass.value.length > 0)) {
-    // another simple step is to verify URL
-    if (checkURL(vaultServer.value)) {
-      // if input exist and URL is ok, attempt authorization to specified vault server URL.
-      chrome.storage.sync.set({ 'vaultAddress': vaultServer.value }, function () { })
-      chrome.storage.sync.set({ 'username': login.value }, function () { })
-      authToVault(vaultServer.value, login.value, pass.value, authMount.value)
-    } else {
-      notify.textContent = 'Bad URL format, please verify'
-    }
+    // if input fields are not empty, attempt authorization to specified vault server URL.
+    chrome.storage.sync.set({ 'vaultAddress': vaultServer.value }, function () { })
+    chrome.storage.sync.set({ 'username': login.value }, function () { })
+    authToVault(vaultServer.value, login.value, pass.value, authMount.value)
   } else {
     notify.textContent = 'Bad input, must fill in all 3 fields.'
   }
